853c503ec7fa43e5ccf29b7c6c4378a92b2bc859
ks4tri4 <faell.ken@gmail.com>
2021-03-02 23:17:31 +0700

    menambah methode kirim pesan

--- wa.py
+++ wa.py
@@ -16,9 +16,29 @@ wait = WebDriverWait(driver, 10)
 wait5 = WebDriverWait(driver, 5)
 
 
-def pilihkontak(nama, driver):
+def pilihkontak(nama):
     user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(nama))
     user.click()
+    print("kontak dipilih")
+    time.sleep(0.5)
+
+
+def kirimpesan(pesan):
+    # Select the Input Box
+    # "//div[@data-tab='6']" adalah identitas xpath untuk menulis pesan balasan
+    # kadang ini dirubah oleh WA, jika tidak berjalan semestinya rubah kode tersebut dengan yang baru
+    inp_xpath = "//div[@data-tab='6']"
+    input_box = wait.until(
+        EC.presence_of_element_located((By.XPATH, inp_xpath)))
+    print("kontak dipilih")
+    time.sleep(1)
+    # Send message
+    # target is your target Name and msgToSend is you message
+    # + Keys.ENTER (Uncomment it if your msg doesnt contain '\n')
+    input_box.send_keys(pesan)
+    # Link Preview Time, Reduce this time, if internet connection is Good
+    time.sleep(2)
+    input_box.send_keys(Keys.ENTER)
 
 
 print("Scan QR Code, And then Enter")
@@ -34,21 +54,9 @@ while (perintah != "exit"):
     if (perintah == "kirim"):
         nama = input("Kontak yang akan dikirim pesan: ")
         try:
-            pilihkontak(nama, driver)
-            # Select the Input Box
-            inp_xpath = "//div[@data-tab='6']"
-            input_box = wait.until(
-                EC.presence_of_element_located((By.XPATH, inp_xpath)))
-            print("kontak dipilih")
-            time.sleep(1)
-            # Send message
-            # target is your target Name and msgToSend is you message
-            # + Keys.ENTER (Uncomment it if your msg doesnt contain '\n')
-            input_box.send_keys("Hello, " + nama + "." + Keys.SHIFT +
-                                Keys.ENTER + "ini pesan auto matis" + Keys.SPACE)
-            # Link Preview Time, Reduce this time, if internet connection is Good
-            time.sleep(3)
-            input_box.send_keys(Keys.ENTER)
+            pilihkontak(nama)
+            pesan = input("tulis pesan -> ")
+            kirimpesan(pesan)
             print("Successfully Send Message to : " + nama + '\n')
             time.sleep(0.5)
 
